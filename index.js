1c57171207e97d72c2115e7872095eadf3ec41a7
Akib <imranulhassan3296@gmail.com>
2024-12-23 00:38:17 +0600

    Added delete endpoint for removing my-apply

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
         res.send(result)
       })
 
+      app.delete('/registration/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await registrationCollection.deleteOne(query);
+        res.send(result)
+      })
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
